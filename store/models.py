3444a043354b780073bc15daa17a18a024714e40
Stilyan Minchev <stilyan.minchev@gmail.com>
2020-12-13 00:04:45 +0200

    small changes

--- store/models.py
+++ store/models.py
@@ -1,9 +1,12 @@
+from django.contrib.auth import get_user_model
 from django.contrib.auth.models import User
 from django.db import models
 
 from store_auth.models import UserProfile
 from store.validators import positive_number
 
+
+# UserModel= get_user_model()
 
 class Watch(models.Model):
     name = models.CharField(max_length=20)
@@ -14,10 +17,11 @@ class Watch(models.Model):
     )
     user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
 
-    #     created_by = models.ForeignKey(User, on_delete=models.CASCADE)
 
-    def __str__(self):
-        return f'{self.name}'
+# user=models.ForeignKey(UserModel, on_delete=models.CASCADE)
+
+def __str__(self):
+    return f'{self.name}'
 
 
 class Like(models.Model):
